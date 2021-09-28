6ce64cbd662ecc0cb3e071077e663fb5a65815e6
Podosinova <mariya.podosinova@accenture.com>
2021-09-28 17:23:24 +0100

    add ExpensesFilter component with lifting state up

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react'
+
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
 
 function Expenses(props) {
+  const [year, setYear] = useState(2021)
+  const filterChangeHandler = (selectedYear) => {
+    setYear(selectedYear)
+  }
   return (
-    <Card className="expenses">
-      {props.expenses.map((el) => (
-        <ExpenseItem title={el.title} date={el.date} amount={el.amount} />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter onChangeFilter={filterChangeHandler} year={year} />
+      <Card className="expenses">
+        {props.expenses.map((el) => (
+          <ExpenseItem title={el.title} date={el.date} amount={el.amount} />
+        ))}
+      </Card>
+    </div>
   )
 }
 
