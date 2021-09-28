7ee4e69ff1b3e5008b0b5c8dd5ef6de7faec62b0
Podosinova <mariya.podosinova@accenture.com>
2021-09-28 17:37:01 +0100

    remove example code btn fron ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from 'react'
 
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
+import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const [title, setTitle]=useState(props.title)
-  const clickHandler = () => {
-    setTitle("hihihi");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
-  );
+  )
 }
 
-export default ExpenseItem;
+export default ExpenseItem
