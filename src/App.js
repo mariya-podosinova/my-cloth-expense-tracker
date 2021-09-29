8f29fd68a971eebf44265ad7d68b74eaa026da9a
Podosinova <mariya.podosinova@accenture.com>
2021-09-29 08:41:35 +0100

    add new item to the list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
 
+const INITIAL_EXPENSES = [
+  {
+    id: 'el1',
+    title: 'Cloth for home wear',
+    amount: 120,
+    date: new Date(2021, 6, 24),
+  },
+  {
+    id: 'el2',
+    title: 'Pyjamas',
+    amount: 60,
+    date: new Date(2021, 7, 24),
+  },
+  {
+    id: 'el3',
+    title: 'Shoes for autumn collection',
+    amount: 50,
+    date: new Date(2021, 8, 24),
+  },
+]
+
 function App() {
-  const expenses = [
-    {
-      id: 'el1',
-      title: 'Cloth for home wear',
-      amount: 120,
-      date: new Date(2021, 6, 24),
-    },
-    {
-      id: 'el2',
-      title: 'Pyjamas',
-      amount: 60,
-      date: new Date(2021, 7, 24),
-    },
-    {
-      id: 'el3',
-      title: 'Shoes for autumn collection',
-      amount: 50,
-      date: new Date(2021, 8, 24),
-    },
-  ]
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES)
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
   }
   return (
     <div>
