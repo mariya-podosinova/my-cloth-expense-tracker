c985246b3d90ac356bdd482d1c2f68f619516264
Podosinova <mariya.podosinova@accenture.com>
2021-09-30 18:00:21 +0100

    show conditional content, for the form buttons

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false)
+  const showFormHandler = () => {
+    setShowForm(!showForm)
+  }
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -11,9 +15,18 @@ const NewExpense = (props) => {
     }
     props.onAddExpense(expenseData)
   }
+  const cancelHandler = () => {
+    setShowForm(false)
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {showForm || <button onClick={showFormHandler}>Add new Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelHandler}
+        />
+      )}
     </div>
   )
 }
